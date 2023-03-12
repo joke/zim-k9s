71e3e015141fb04f0d9438dab37e7ef3d0ed9834
Joke de Buhr <joke@xckk.de>
2023-03-12 16:14:58 +0100

    fix: new version detection message
    Signed-off-by: Joke de Buhr <joke@xckk.de>

--- init.zsh
+++ init.zsh
@@ -6,6 +6,6 @@
   local compfile=$1/functions/_k9s
   if [[ ! -e $compfile || $compfile -ot $command ]]; then
     $command completion zsh >| $compfile
-    print -u2 -PR "* Detected a new version 'k9s'. Regenerated completions.
+    print -u2 -PR "* Detected a new version 'k9s'. Regenerated completions."
   fi
 } ${0:h}
